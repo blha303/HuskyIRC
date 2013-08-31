8952966a4c8b0dc8a4841a5a2cd4fae61474f4bf
Husky-- <ttghusky@live.com.au>
2013-08-31 23:01:41 +1000

    Update Config.java

--- src/com/huskehhh/code/Config.java
+++ src/com/huskehhh/code/Config.java
@@ -7,12 +7,6 @@ import java.util.Properties;
 
 public class Config {
     
-    public static String hostname;
-    public static String port;
-    public static String user;
-    public static String password;
-    public static String database;
-    
     public static String Ohostname;
     public static String Oport;
     public static String Ouser;
@@ -27,12 +21,6 @@ public class Config {
         
         Properties config = new Properties();
         config.load(new FileInputStream("HuskyIRC.properties"));
-        
-        hostname = config.getProperty("MySQL-host");
-        port = config.getProperty("MySQL-port");
-        user = config.getProperty("MySQL-user");
-        password = config.getProperty("MySQL-password");
-        database = config.getProperty("MySQL-database");
         
         network = config.getProperty("irc-network");
         nickPass = config.getProperty("nickserv-pass");
