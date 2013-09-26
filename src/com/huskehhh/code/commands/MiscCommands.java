125f623ef58b56f4e5e93b70de9e218311ddbdb3
Huskehhh <ttghusky@live.com.au>
2013-09-26 17:00:16 +1000

    Minor fixes for MiscCommands

--- src/com/huskehhh/code/commands/MiscCommands.java
+++ src/com/huskehhh/code/commands/MiscCommands.java
@@ -7,54 +7,55 @@ import org.pircbotx.hooks.events.MessageEvent;
 public class MiscCommands extends ListenerAdapter {
 
     public void onMessage(MessageEvent event) {
+        if (event.getMessage().length() == 1) {
+            String[] line = event.getMessage().split(" ");
 
-        String[] line = event.getMessage().split(" ");
-
-        if (event.getMessage().startsWith("!c ")) {
-            if (line[1] != null) {
-                event.getBot().sendAction(event.getChannel(), "gives " + line[1] + " a cookie!");
-            } else {
-                event.respond("You need to actually have a paramater after the command! >.<");
+            if (event.getMessage().startsWith("!c ")) {
+                if (line[1] != null) {
+                    event.getBot().sendAction(event.getChannel(), "gives " + line[1] + " a cookie!");
+                } else {
+                    event.respond("You need to actually have a paramater after the command! >.<");
+                }
+            } else if (event.getMessage().startsWith("!pizza ")) {
+                if (line[1] != null) {
+                    event.getBot().sendAction(event.getChannel(), "gives " + line[1] + " a pizza!");
+                } else {
+                    event.respond("You need to actually have a parameter after the command! >.<");
+                }
+            } else if (event.getMessage().startsWith("!slap ")) {
+                if (line[1] != null) {
+                    event.getBot().sendAction(event.getChannel(), "slaps " + line[1] + "!");
+                } else {
+                    event.respond("You need to actually have a parameter after the command! >.<");
+                }
+            } else if (event.getMessage().startsWith("!slay ")) {
+                if (line[1] != null) {
+                    event.getBot().sendAction(event.getChannel(), "slays " + line[1] + "!");
+                } else {
+                    event.respond("You need to actually have a parameter after the command! >.<");
+                }
+            } else if (event.getMessage().startsWith("!eat ")) {
+                if (line[1] != null) {
+                    event.getBot().sendAction(event.getChannel(), "eats " + line[1] + "!");
+                } else {
+                    event.respond("You need to actually have a parameter after the comand! >.<");
+                }
+            } else if (event.getMessage().startsWith("!hf ")) {
+                if (line[1] != null) {
+                    event.getBot().sendAction(event.getChannel(), "gives " + line[1] + " a hi-five!");
+                } else {
+                    event.respond("You need to actually have a parameter after the command! >.<");
+                }
+            } else if (event.getMessage().startsWith("\\o")) {
+                event.respond("o/");
+            } else if (event.getMessage().startsWith("o/")) {
+                event.respond("\\o");
+            } else if (event.getMessage().equalsIgnoreCase("HuskBot: Is Smithey someone's bitch?")) {
+                event.respond("Once upon a there was a person named Smithey, He went around saying he would finish stuff, but in actual fact he didn't.");
+                event.respond("One day the almighty Zachoz learned that incentives would make him become his bitch.");
+                event.respond("And to this day he is still the bitch of the almighty Zachoz!");
+                event.respond("The End!");
             }
-        } else if (event.getMessage().startsWith("!pizza")) {
-            if (line[1] != null) {
-                event.getBot().sendAction(event.getChannel(), "gives " + line[1] + " a pizza!");
-            } else {
-                event.respond("You need to actually have a parameter after the command! >.<");
-            }
-        } else if (event.getMessage().startsWith("!slap")) {
-            if (line[1] != null) {
-                event.getBot().sendAction(event.getChannel(), "slaps " + line[1] + "!");
-            } else {
-                event.respond("You need to actually have a parameter after the command! >.<");
-            }
-        } else if (event.getMessage().startsWith("!slay")) {
-            if (line[1] != null) {
-                event.getBot().sendAction(event.getChannel(), "slays " + line[1] + "!");
-            } else {
-                event.respond("You need to actually have a parameter after the command! >.<");
-            }
-        } else if (event.getMessage().startsWith("!eat")) {
-            if (line[1] != null) {
-                event.getBot().sendAction(event.getChannel(), "eats " + line[1] + "!");
-            } else {
-                event.respond("You need to actually have a parameter after the comand! >.<");
-            }
-        } else if (event.getMessage().startsWith("!hf")) {
-            if (line[1] != null) {
-                event.getBot().sendAction(event.getChannel(), "gives " + line[1] + " a hi-five!");
-            } else {
-                event.respond("You need to actually have a parameter after the command! >.<");
-            }
-        } else if (event.getMessage().startsWith("\\o")) {
-            event.respond("o/");
-        } else if (event.getMessage().startsWith("o/")) {
-            event.respond("\\o");
-        } else if (event.getMessage().equalsIgnoreCase("HuskBot: Is Smithey someones bitch?")) {
-            event.respond("Once upon a there was a person named Smithey, He went around saying he would finish stuff, but in actual fact he didn't.");
-            event.respond("One day the almighty Zachoz learned that incentives would make him become his bitch.");
-            event.respond("And to this day he is still the bitch of the almighty Zachoz!");
-            event.respond("The End!!");
         }
     }
 
