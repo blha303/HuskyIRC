7260b24f339e39016e62a0d9f762fecf248d7e49
Husky <ttghusky@live.com.au>
2013-11-12 20:11:34 +1100

    Add quick message to tell me it updated!

--- src/com/huskehhh/code/commands/misc/Update.java
+++ src/com/huskehhh/code/commands/misc/Update.java
@@ -52,6 +52,7 @@ public class Update extends ListenerAdapter {
 
                         try {
                             Runtime.getRuntime().exec("./updateMapREPO.sh");
+                            event.respond("Updated!");
                         } catch (IOException exception) {
                             exception.printStackTrace();
                             event.respond("Error updating! Check console!");
