13fef738f786020f1854628a0cc6031835bc0113
Huskehhh <ttghusky@live.com.au>
2013-12-04 15:52:16 +1100

    Use bot API for Roles

--- src/com/huskehhh/code/commands/core/Permissions.java
+++ src/com/huskehhh/code/commands/core/Permissions.java
@@ -2,6 +2,7 @@ package com.huskehhh.code.commands.core;
 
 import com.huskehhh.code.HuskyIRC;
 import com.huskehhh.code.auth.AuthCheck;
+import com.huskehhh.code.util.Utility;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
 
@@ -13,19 +14,19 @@ public class Permissions extends ListenerAdapter {
 
         if (line[0].equalsIgnoreCase("!op") && AuthCheck.authCheck(event.getUser().getNick())) {
 
-            HuskyIRC.bot.sendRawLineNow("/msg chanserv flags " + event.getChannel().getName() + " " + line[1] + " AOP");
+            HuskyIRC.bot.op(event.getChannel(), Utility.getUser(line[1], event.getChannel().getName()));
 
         } else if (line[0].equalsIgnoreCase("!deop") && AuthCheck.authCheck(event.getUser().getNick())) {
 
-            HuskyIRC.bot.sendRawLineNow("/msg chanserv flags " + event.getChannel().getName() + " " + line[1] + " -*");
+            HuskyIRC.bot.deOp(event.getChannel(), Utility.getUser(line[1], event.getChannel().getName()));
 
         } else if (line[0].equalsIgnoreCase("!voice") && AuthCheck.authCheck(event.getUser().getNick())) {
 
-            HuskyIRC.bot.sendRawLineNow("/msg chanserv flags " + event.getChannel().getName() + " " +  line[1] + " VOP");
+            HuskyIRC.bot.voice(event.getChannel(), Utility.getUser(line[1], event.getChannel().getName()));
 
         } else if (line[0].equalsIgnoreCase("!devoice") && AuthCheck.authCheck(event.getUser().getNick())) {
 
-            HuskyIRC.bot.sendRawLineNow("/msg chanserv flags " + event.getChannel().getName() + " " +  line[1] + " -*");
+            HuskyIRC.bot.deVoice(event.getChannel(), Utility.getUser(line[1], event.getChannel().getName()));
 
         }
 
