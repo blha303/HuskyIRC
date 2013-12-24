06fd733b9c4c2ef66b3969cc204f16e2ccdc57d5
Huskehhh <ttghusky@live.com.au>
2013-12-24 15:58:14 +1100

    Add BattleStats as a listener

--- src/com/huskehhh/code/HuskyIRC.java
+++ src/com/huskehhh/code/HuskyIRC.java
@@ -75,6 +75,7 @@ public class HuskyIRC {
         bot.getListenerManager().addListener(new RotationGen());
         bot.getListenerManager().addListener(new Permissions());
         bot.getListenerManager().addListener(new Punishment());
+        bot.getListenerManager().addListener(new BattleStats());
     }
 
 
