8e4c8987a008dd64758cd37afc3fe427c4881229
Huskehhh <ttghusky@live.com.au>
2014-01-22 12:30:28 +1100

    Use a proper list for recent tweets..

--- src/com/huskehhh/code/tasks/TwitterCheckTask.java
+++ src/com/huskehhh/code/tasks/TwitterCheckTask.java
@@ -7,7 +7,7 @@ import com.huskehhh.code.util.Utility;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
-import java.awt.*;
+import java.util.List;
 import java.util.ListIterator;
 import java.util.TimerTask;
 
@@ -26,7 +26,7 @@ public class TwitterCheckTask extends TimerTask {
         int tweetCount = 1;
         long userId = 763736701; //OresomeCraft Twitter User ID
 
-        ResponseList tweet = null;
+        List<Status> tweet = null;
 
         try {
             tweet = twitter.getUserTimeline(userId, new Paging(1, tweetCount));
@@ -39,7 +39,7 @@ public class TwitterCheckTask extends TimerTask {
         while (li.hasNext()) {
             String next = li.next().toString();
             if (!TwitterUtil.lastPost.equals(next)) {
-                HuskyIRC.bot.sendMessage("#oresomecraft", Color.GREEN + "OresomeCraft Twitter: " + next);
+                HuskyIRC.bot.sendMessage("#oresomecraft", "OresomeCraft Twitter: " + next);
                 TwitterUtil.lastPost = li.next().toString();
             } else {
                 Utility.getUser("Husk", "Debug: " + li.next().toString() + " || " + TwitterUtil.lastPost);
