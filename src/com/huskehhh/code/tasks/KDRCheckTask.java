4cef59e0a9aa5633289eccc956ce6ecf231890cf
ScruffyRules <scott-scheiner@hotmail.com>
2014-03-02 15:38:16 +1030

    Add zachoz to me

--- src/com/huskehhh/code/tasks/KDRCheckTask.java
+++ src/com/huskehhh/code/tasks/KDRCheckTask.java
@@ -7,8 +7,8 @@ public class KDRCheckTask extends TimerTask {
     @Override
     public void run() {
 
-      //KDRUtil.sendKD("MinecraftUsername", "IRCUsername");
-        KDRUtil.sendKD("ScruffyRules,_Husky_,dutchy336,psgs,SuperDuckFace", "Scruffeh,Scruff,ScruffDoge");
+      //KDRUtil.sendKD("MinecraftUsername,etc", "IRCUsername,etc");
+        KDRUtil.sendKD("ScruffyRules,_Husky_,dutchy336,psgs,SuperDuckFace,zachoz", "Scruffeh,Scruff,ScruffDoge");
         KDRUtil.sendKD("123Oblivious", "Obbehhh,Obby");
         KDRUtil.sendKD("SuperDuckFace,dutchy336", "SuperDuckFace_,SuperDuckFace");
         KDRUtil.sendKD("psgs", "psgs");
