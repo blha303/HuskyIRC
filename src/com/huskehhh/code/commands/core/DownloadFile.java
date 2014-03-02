b25dab9c372811978e69222946685edbefef90a7
ScruffyRules <scott-scheiner@hotmail.com>
2014-03-02 23:58:51 +1030

    Yay for late night programming!

--- src/com/huskehhh/code/commands/core/DownloadFile.java
+++ src/com/huskehhh/code/commands/core/DownloadFile.java
@@ -10,21 +10,29 @@ public class DownloadFile extends ListenerAdapter {
 
         String[] line = event.getMessage().split(" ");
 
-        if (line.length > 2) {
-            if (Utility.isAdmin(event.getUser().getNick())) {
+        if (line[0].equalsIgnoreCase("!dl")) {
 
-                event.respond("Downloading " + line[1] + "!");
-                long startTime = System.currentTimeMillis();
-                Utility.downloadFile(line[1], line[2]);
-                long endTime = System.currentTimeMillis();
-                event.respond("Download finished!");
-                event.respond("Downloaded in " + (startTime - endTime) + "ms");
+            if (line.length > 2) {
+
+                if (Utility.isAdmin(event.getUser().getNick())) {
+
+                    event.respond("Downloading " + line[1] + "!");
+                    long startTime = System.currentTimeMillis();
+                    Utility.downloadFile(line[1], line[2]);
+                    long endTime = System.currentTimeMillis();
+                    event.respond("Download finished!");
+                    event.respond("Downloaded in " + (endTime - startTime) + "ms");
+                } else {
+
+                    event.respond("Sorry you can't use this!");
+                }
             } else {
-                event.respond("Sorry you can't use this!");
+
+                event.respond("Syntax: !dl <url> <filename>.<ext>");
             }
-        } else {
-            event.respond("Syntax: !dl <url> <filename>.<ext>");
+
         }
+
     }
 
 }
