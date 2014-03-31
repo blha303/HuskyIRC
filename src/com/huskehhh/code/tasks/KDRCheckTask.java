28cec5f8d913fbf35eaf97b7e334374a91191165
ScruffyRules <scott-scheiner@hotmail.com>
2014-03-31 11:22:41 +1030

    Remove debug

--- src/com/huskehhh/code/tasks/KDRCheckTask.java
+++ src/com/huskehhh/code/tasks/KDRCheckTask.java
@@ -9,21 +9,13 @@ public class KDRCheckTask extends TimerTask {
     public void run() {
 
       //KDRUtil.sendKD("MinecraftUsername,etc", "IRCUsername,etc");
-        System.out.println("KDRCheck: Start!");
-        long startTime = System.currentTimeMillis();
-        
         KDRUtil.sendKD("ScruffyRules", "Scruffeh,Scruff,ScruffDoge");
         KDRUtil.sendKD("_Husky_", "Husk");
         KDRUtil.sendKD("123Oblivious", "Obby,Obbehhh");
         KDRUtil.sendKD("SuperDuckFace,dutchy336", "SuperDuckFace_");
         KDRUtil.sendKD("psgs", "psgs");
-        
-        System.out.println("KDRCheck: End!");
-        System.out.println("KDRCheck took " + (System.currentTimeMillis() - startTime) + "ms");
-        
-        startTime = System.currentTimeMillis();
-        KDRUtil.updateUserKD("ScruffyRules,_Husky_,dutchy336,psgs,SuperDuckFace,123Oblivious");
-        System.out.println("KDRUpdate took " + (System.currentTimeMillis() - startTime) + "ms");
+
+        KDRUtil.updateUserKD("ScruffyRules,_Husky_,SuperDuckFace,dutchy336,psgs,123Oblivious");
 
         new KDRCheck();
     }
