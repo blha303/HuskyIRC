129efeafc7d9ed8c101484122eab2ad53905ec51
Huskehhh <husky@oresomecraft.com>
2014-04-26 20:22:49 +1000

    Oops- Fix up methods

--- src/com/huskehhh/code/commands/oresomecraft/GlobalCount.java
+++ src/com/huskehhh/code/commands/oresomecraft/GlobalCount.java
@@ -20,13 +20,13 @@ public class GlobalCount extends ListenerAdapter {
         String[] line = event.getMessage().split(" ");
 
         if (line[0].equalsIgnoreCase("!globalcount") || line[0].equalsIgnoreCase("!gcount")) {
-            int hub = Utility.getPlayersOnline("hub");
-            int smp = Utility.getPlayersOnline("smp");
-            int battle = Utility.getPlayersOnline("battle");
-            int arcade = Utility.getPlayersOnline("arcade");
-            int kart = Utility.getPlayersOnline("oresomekart");
-            int tiot = Utility.getPlayersOnline("tiot");
-            int dev = Utility.getPlayersOnline("dev");
+            int hub = getPlayersOnline("hub");
+            int smp = getPlayersOnline("smp");
+            int battle = getPlayersOnline("battle");
+            int arcade = getPlayersOnline("arcade");
+            int kart = getPlayersOnline("oresomekart");
+            int tiot = getPlayersOnline("tiot");
+            int dev = getPlayersOnline("dev");
             String channel = event.getChannel().getName();
             event.getBot().sendMessage(channel, "--== OresomeCraft Global Player Count ==--");
             event.respond("Hub: " + hub);
