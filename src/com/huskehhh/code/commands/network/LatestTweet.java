c9c37341caeba7cf361894e8e2add377911b2af8
ScruffyRules <scott-scheiner@hotmail.com>
2014-05-06 21:24:07 +0930

    Make !lasttweet fail safe. (Maybe?)

--- src/com/huskehhh/code/commands/network/LatestTweet.java
+++ src/com/huskehhh/code/commands/network/LatestTweet.java
@@ -3,8 +3,7 @@ package com.huskehhh.code.commands.network;
 import com.huskehhh.code.config.Config;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
-import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
+import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class LatestTweet extends ListenerAdapter {
@@ -22,7 +21,14 @@ public class LatestTweet extends ListenerAdapter {
                     .setOAuthAccessTokenSecret(Config.oAuthTokenSecret);
                 TwitterFactory twitterFactory = new TwitterFactory(configurationBuilder.build());
                 Twitter twitter = twitterFactory.getInstance();
-                event.respond(twitter.getUserTimeline(line[1]).get(0).getText());
+                try {
+                    ResponseList<Status> timeLine = twitter.getUserTimeline(line[1]);
+                    event.respond(timeLine.get(0).getUser().getName() + ": " + timeLine.get(0).getText());
+                } catch (TwitterException ex) {
+                    event.respond(ex.getCause().toString());
+                }
+            } else {
+                event.respond("Usage: !lasttweet <twitter username>");
             }
         }
     }
