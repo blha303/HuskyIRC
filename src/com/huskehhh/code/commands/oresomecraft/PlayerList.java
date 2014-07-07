04c9313b56662d8fde0e7638772c5490241fb0c7
Huskehhh <ttghusky@live.com.au>
2014-07-07 19:14:06 +1000

    Start finishing !players

--- src/com/huskehhh/code/commands/oresomecraft/PlayerList.java
+++ src/com/huskehhh/code/commands/oresomecraft/PlayerList.java
@@ -7,6 +7,8 @@ import org.pircbotx.hooks.events.MessageEvent;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlayerList extends ListenerAdapter {
 
@@ -19,24 +21,42 @@ public class PlayerList extends ListenerAdapter {
         String[] line = event.getMessage().split(" ");
 
         if (line[0].equalsIgnoreCase("!players")) {
-            if (line.length == 1) {return;}
-//            String players = "";
-            String server = line[1];
-            String query = "SELECT * FROM `online_users`.`players` WHERE server LIKE '" + server + "';";
-            ResultSet rs = mysql.querySQL(query);
-            try {
-                while (rs.next()) {
-                    while (rs.getString("user") != null) {
-//                        players += rs.getString("user") + ", ";
-                        event.respond(rs.getString("user"));
-                        rs.next();
-                    }
+            if (line.length == 1) {
+                if (event.getChannel().equals("#oresomecraft")) {
+                    return getAllPlayersOnline();
                 }
-//                event.respond(players);
-            } catch (SQLException e) {
-                e.printStackTrace();
+            }
+            String players = "";
+            String server = line[1];
+            if (server.equalsIgnoreCase("smp") || server.equalsIgnoreCase("battle") || server.equalsIgnoreCase("arcade") || server.equalsIgnoreCase("tiot") || server.equalsIgnoreCase("hub") || server.equalsIgnoreCase("hub")) {
+                String query = "SELECT * FROM `online_users`.`players` WHERE server LIKE '" + server + "';";
+                String count = "SELECT COUNT(*) FROM `online_users`.`players` WHERE server LIKE '" + server + "';";
+                ResultSet rs = mysql.querySQL(query);
+                ResultSet rsc = mysql.querySQL(count);
+
+                int rows = 0;
+
+                try {
+                    if (rsc.next()) {
+                        rows = rsc.getInt("COUNT(*)");
+                    }
+                    for (int i = 0; i < rows; i++) {
+                        players += rs.getString(i);
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                event.respond(players);
             }
         }
     }
 
+    public String getAllPlayersOnline() {
+        String players = "";
+        String[] servers = {"smp", "battles", "arcade"};
+
+        ResultSet smprs = mysql.querySQL(smp);
+        ResultSet smpcount = mysql.querySQL();
+        return players;
+    }
 }
