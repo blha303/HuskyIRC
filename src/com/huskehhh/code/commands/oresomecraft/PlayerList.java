fce8c7656c17942961feaa9dfe27f99a073ba42a
Huskehhh <ttghusky@live.com.au>
2014-07-08 12:34:27 +1000

    Fix the output

--- src/com/huskehhh/code/commands/oresomecraft/PlayerList.java
+++ src/com/huskehhh/code/commands/oresomecraft/PlayerList.java
@@ -1,6 +1,5 @@
 package com.huskehhh.code.commands.oresomecraft;
 
-import com.huskehhh.code.HuskyIRC;
 import com.huskehhh.code.config.Config;
 import com.huskehhh.database.mysql.MySQL;
 import org.pircbotx.hooks.ListenerAdapter;
@@ -8,8 +7,6 @@ import org.pircbotx.hooks.events.MessageEvent;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class PlayerList extends ListenerAdapter {
 
@@ -23,34 +20,9 @@ public class PlayerList extends ListenerAdapter {
 
         if (line[0].equalsIgnoreCase("!players")) {
             if (line.length == 1) {
-                if (event.getChannel().getName().equals("#oresomecraft")) {
-                    event.respond(getAllPlayersOnline());
-                }
+                parseResultSet(line[0]);
             } else {
-                String players = "";
-                String server = line[1];
-                if (server.equalsIgnoreCase("smp") || server.equalsIgnoreCase("battle") || server.equalsIgnoreCase("arcade") || server.equalsIgnoreCase("tiot") || server.equalsIgnoreCase("hub") || server.equalsIgnoreCase("dev")) {
-                    String query = "SELECT * FROM `online_users`.`players` WHERE server LIKE '" + server + "';";
-                    String count = "SELECT COUNT(*) FROM `online_users`.`players` WHERE server LIKE '" + server + "';";
-                    ResultSet rs = mysql.querySQL(query);
-                    ResultSet rsc = mysql.querySQL(count);
-
-                    int rows = 0;
-
-                    try {
-                        if (rsc.next()) {
-                            rows = rsc.getInt("COUNT(*)");
-                            for (int i = 1; i < rows; i++) {
-                                System.out.println("i=" + i + " and rows=" + rows);
-                                rs.absolute(i);
-                                players += rs.getString("user") + ", ";
-                            }
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    event.respond(players.substring(0, players.length()-2));
-                }
+                parseResultSet("all");
             }
         }
     }
@@ -60,27 +32,40 @@ public class PlayerList extends ListenerAdapter {
      * 'players' format,
      * SMP: player, player | Battles: player, player | Arcade: player, player | TiOT: player, player | Hub: player, player
      */
-    public String getAllPlayersOnline() {
-        String players = "";
-        String[] servers = {"SMP", "Battle", "Arcade", "Hub", "TiOT"};
 
-        for (int i = 0; i < servers.length; i++) {
-            ResultSet rs = mysql.querySQL("SELECT * FROM `online_users`.`players` WHERE server LIKE '" + servers[i] + "';");
-            ResultSet count = mysql.querySQL("SELECT COUNT(*) FROM `online_users`.`players` WHERE server LIKE '" + servers[i] + "';");
+    public String parseResultSet(String server) {
+        if (server.equalsIgnoreCase("smp") || server.equalsIgnoreCase("battle") || server.equalsIgnoreCase("arcade") || server.equalsIgnoreCase("tiot") || server.equalsIgnoreCase("hub") || server.equalsIgnoreCase("dev")) {
+            String players = "";
+            String query = "SELECT * FROM `online_users`.`players` WHERE server LIKE '" + server + "';";
+            String count = "SELECT COUNT(*) FROM `online_users`.`players` WHERE server LIKE '" + server + "';";
+            ResultSet rs = mysql.querySQL(query);
+            ResultSet rsc = mysql.querySQL(count);
+
             int rows = 0;
+
             try {
-                if (count.next()) {
-                    rows = count.getInt("COUNT(*)");
-                    players += " | " + servers[i] + ": ";
-                    for (int x = 1; x < rows; x++) {
-                        rs.absolute(x);
-                        players += rs.getString(x) + ", ";
+                if (rsc.next() && rs.next()) {
+                    rows = rsc.getInt("COUNT(*)");
+                    for (int i = 1; i < rows; i++) {
+                        rs.absolute(i);
+                        players += rs.getString("user") + ", ";
                     }
+                } else {
+                    return "No players online!";
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+            return players;
+        } else {
+            String smp = parseResultSet("smp");
+            String battle = parseResultSet("battle");
+            String arcade = parseResultSet("arcade");
+            String hub = parseResultSet("hub");
+            String dev = parseResultSet("dev");
+            String tiot = parseResultSet("tiot");
+
+            return "Players online: | SMP: " + smp + "| Battle: " + battle + "| Arcade: " + arcade + "| Hub: " + hub + "| Dev: " + dev + " | TiOT: " + tiot + "|";
         }
-        return players;
     }
 }
